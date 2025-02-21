ffd9696ef42ae36e157f9a9d493a7858da91c261
ennyolar96 <ennyolar1996@gmail.com>
2025-02-21 10:00:18 +0100

    added default landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 import authRoute from "./route";
 
 export const applicationRoute = (app: Application) => {
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+  });
   app.use("/api/v1", authRoute);
 };
